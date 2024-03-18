951fba5e514ce765aaad994adb85e16b184c6fc8
nourhan <nourhan@backbase.com>
2024-03-18 01:12:02 +0100

    Add A11y Tests

---
+++ ReferenceiOSUITests/A11yTests/MainScreenTests.swift
@@ -0,0 +1,9 @@
+//
+//  MainScreenTests.swift
+//  ReferenceiOSUITests
+//
+//  Created by Nourhan Mohamed on 18/03/2024.
+//  Copyright © 2024 ABN AMRO. All rights reserved.
+//
+
+import Foundation
